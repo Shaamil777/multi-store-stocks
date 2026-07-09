0afd5f46e8c67fb13c6dc14f09c46344a4750193
Shaamil777 <shamilm2324@gmail.com>
2026-07-10 04:21:19 +0530

    feat:added auth middleware for authentication

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+
+const authMiddleware = (req,res,next)=>{
+    try {
+        const authHeader = req.headers.authorization
+        if(!authHeader){
+            return res.status(401).json({
+                success:false,
+                message:"Access denied. No token Provided"
+            })
+        }
+        
+        const token = authHeader.split(" ")[1]
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:"invalid or expired token"
+        })
+    }
+}
+
+module.exports = authMiddleware
